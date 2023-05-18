012512077cbc02727189a7ebb27cc4e63e0568a1
Kuanysh <developerandroidkz@gmail.com>
2023-05-18 16:40:16 +0500

    fix bug

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\CategoryService;
+
 class CategoryController extends BaseController
 {
-    //
+    /**
+     * @param App\Http\Services\CategoryService $service
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function list(CategoryService $service)
+    {
+        return $this->success($service->list());
+    }
 }
